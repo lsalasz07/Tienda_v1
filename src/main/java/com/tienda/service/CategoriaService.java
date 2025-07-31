6744d363010e5335c600446a63744990aad7de30
lsalasz07 <luisignaciosalassalazar19@gmail.com>
2025-07-31 12:19:05 -0600

    Semana 6

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -7,5 +7,11 @@ import java.util.List;
 public interface CategoriaService {
     //asigna el metodo para despues implementarlo en el serviceImpl (Read)
     public List<Categoria> getCategorias(boolean activos);
+    
+    public Categoria getCategoria(Categoria categoria);
+    
+    public void save(Categoria categoria);
+    
+    public void delete(Categoria categoria);
 }
  
